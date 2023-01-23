7b9bf3542de0d29169633aea35c191fb42d4588d
Mattia Panni <mattia.panni@studio.unibo.it>
2023-01-23 18:37:15 +0100

    Fix bugs

--- src/createsquad.php
+++ src/createsquad.php
@@ -2,7 +2,7 @@
 require_once 'bootstrap.php';
 
 if($dbh->createSquad($_POST['name'], $_POST['description'], $_SESSION['username'])){
-    header("Location: squadpage.php");
+    header("Location: squadpage.php?name=" . $_POST['name']);
 } else {
     header("Location: createsquad.php?error=1");
 }
