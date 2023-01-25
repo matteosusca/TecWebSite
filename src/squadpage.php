1d563f754a098c022561cb2e81de9e77d4a30f6c
Mattia Panni <mattia.panni@studio.unibo.it>
2023-01-25 18:55:49 +0100

    Add html+bootstrap ui to squadpage

--- src/squadpage.php
+++ src/squadpage.php
@@ -16,33 +16,107 @@ else{
     header("Location: squadpage.php?error=2");
 }
 ?>
+
+
+
+
+<?php require_once 'checkSession.php'; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
+
 <head>
-    <meta charset="UTF-8">
-    <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
+    <title><?php echo $title; ?></title>
 </head>
-<body>
-    <div>
-        <img src=<?php if($squadProfile->getPicture() != null) {
-            echo $dbh->getMediaUrl($squadProfile->getPicture());
-        } else {
-            echo "img/ciccio.jpg";
-        } ?> alt="Profile picture">
-        <div>
-            <h1><?php echo $squadProfile->getId(); ?></h1> <!-- debug purupose -->
-            <h2><?php echo $squadProfile->getName(); ?></h2>
-            <h3><?php echo $squadProfile->getDescription(); ?></h3>
-            <h3><?php echo $squadProfile->getOwner(); ?></h3>
-            <h3><ul><?php 
-            foreach($squadProfile->getMembers() as $member) {
-                echo "<li>".$member."</li>";
-            }
-            ?></ul></h3>
+
+<body class="d-flex flex-column vh-100 " data-bs-theme="dark">
+    <nav class="navbar navbar-icon-top navbar-expand-lg shadow-sm bg-black navbar-dark">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
+                aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">
+                <h2> SquadUp </h2>
+            </a>
+            <div class="dropdown z-2 order-lg-1 ">
+                <a href="#" class=" link-light text-decoration-none dropdown-toggle" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle">utente
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end text-small shadow ">
+                    <li><a class="dropdown-item" href="signin.php">New project...</a></li>
+                    <li><a class="dropdown-item" href="#">Settings</a></li>
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="signout.php">Sign out</a></li>
+                </ul>
+            </div>
+            <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
+                <ul
+                    class="navbar-nav text-center w-100 mx-2 d-flex flex-row justify-content-lg-center justify-content-around">
+                    <li class="nav-item">
+                        <a class="nav-link active " href="index.php"><i class="bi bi-house d-block "
+                                style="font-size: 1rem;"></i>Home</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " href="#"><i class="bi bi-people d-block"
+                                style="font-size: 1rem;"></i>Group</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " href="contacts.php"><i class="bi bi-people d-block"
+                                style="font-size: 1rem;"></i>Contacts</a>
+                    </li>
+
+                </ul>
+
+                <form class="mx-2" role="search">
+                    <input class="form-control bg-body" type="search" placeholder="Search" aria-label="Search">
+                </form>
+
+
+            </div>
+
+
         </div>
-    </div>
+
+    </nav>
+
+
+
+    <main class="d-lg-flex flex-wrap justify-content-center overflow-auto vh-100">
+        <aside
+            class="col-12 col-lg-3 p-3 shadow sticky-lg-top mh-100 overflow-auto d-flex flex-lg-column text-nowrap z-1">
+            
+        </aside>
+        <div class="col-12 col-lg-5 p-3 shadow">
+
+        </div>
+        
+    </main>
+
 </body>
 
+
 </html>
+
+
+
+
+
+
+
+
+
+
+
