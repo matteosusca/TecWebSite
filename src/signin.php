49cd71cc061052272e27a123b6ad534d3b46c5e7
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-01-25 19:06:31 +0100

    fix canvas

--- src/signin.php
+++ src/signin.php
@@ -11,7 +11,6 @@ if (!empty($_POST['submit'])) {
     } else {
         header("Location: login.php?error=1");
     }
-
 }
 
 
@@ -23,8 +22,7 @@ if (!empty($_POST['submit'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/style.css">
     <title>SquadUp</title>
@@ -33,8 +31,7 @@ if (!empty($_POST['submit'])) {
 <body class="d-flex flex-column vh-100 " data-bs-theme="dark">
 
     <nav class="navbar navbar-icon-top navbar-expand-lg shadow-sm bg-black navbar-dark ">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
-            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <header class="navbar-brand mx-5">
@@ -43,24 +40,19 @@ if (!empty($_POST['submit'])) {
         <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
             <ul class="navbar-nav mx-auto text-center">
                 <li class="nav-item">
-                    <a class="nav-link  " href="index.php"><i class="bi bi-house d-md-inline d-lg-block"
-                            style="font-size: 1rem;"></i>Home</a>
+                    <a class="nav-link  " href="index.php"><i class="bi bi-house d-md-inline d-lg-block" style="font-size: 1rem;"></i>Home</a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link " href="#"><i class="bi bi-people d-md-inline d-lg-block"
-                            style="font-size: 1rem;"></i>Group</a>
+                    <a class="nav-link " href="#"><i class="bi bi-people d-md-inline d-lg-block" style="font-size: 1rem;"></i>Group</a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link " href="contacts.php"><i class="bi bi-people d-md-inline d-lg-block"
-                            style="font-size: 1rem;"></i>Contacts</a>
+                    <a class="nav-link " href="contacts.php"><i class="bi bi-people d-md-inline d-lg-block" style="font-size: 1rem;"></i>Contacts</a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link active" href="signin.php"><i class="bi bi-people d-md-inline d-lg-block"
-                            style="font-size: 1rem;"></i>Sign in</a>
+                    <a class="nav-link active" href="signin.php"><i class="bi bi-people d-md-inline d-lg-block" style="font-size: 1rem;"></i>Sign in</a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link " href="signup.php"><i class="bi bi-people d-md-inline d-lg-block"
-                            style="font-size: 1rem;"></i>Sign up</a>
+                    <a class="nav-link " href="signup.php"><i class="bi bi-people d-md-inline d-lg-block" style="font-size: 1rem;"></i>Sign up</a>
                 </li>
             </ul>
 
@@ -94,9 +86,7 @@ if (!empty($_POST['submit'])) {
             </div>
         </div>
     </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
 
