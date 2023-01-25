a4d58cd9bd74edba05500e81414dc4b5dc1665f5
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-01-25 20:01:17 +0100

    fix footer of post

--- src/index.php
+++ src/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once 'bootstrap.php';
 require_once 'checkSession.php';
 ?>
@@ -11,22 +11,19 @@ require_once 'checkSession.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-        crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="css/style.css">
     <title>SquadUp</title>
 </head>
 
 <body class="d-flex flex-column vh-100 " data-bs-theme="dark">
-    <header class="navbar navbar-icon-top navbar-expand-lg shadow-sm bg-black navbar-dark">
+    <header class="navbar navbar-icon-top navbar-expand-lg shadow-sm bg-black navbar-dark ">
         <div class="container">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
                 aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="index.php">
-                <h2> SquadUp </h2>
+            <a class="navbar-brand" href="index.php">SquadUp
             </a>
             <div class="dropdown z-2 order-lg-1 ">
                 <a href="#" class=" link-light text-decoration-none dropdown-toggle" data-bs-toggle="dropdown"
@@ -45,7 +42,7 @@ require_once 'checkSession.php';
             </div>
             <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
                 <ul
-                    class="navbar-nav text-center w-100 mx-2 d-flex flex-row justify-content-lg-center justify-content-around">
+                    class="navbar-nav text-center w-100 mx-2 d-flex flex-row flex-wrap justify-content-lg-center justify-content-evenly">
                     <li class="nav-item">
                         <a class="nav-link  <?php isActive(" index.php"); ?>" href="index.php"><i
                                 class="bi bi-house d-block " style="font-size: 1rem;"></i>Home</a>
@@ -91,20 +88,34 @@ require_once 'checkSession.php';
                 <img src="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg"
                     class="object-fit-contain" alt="..." height="455" />
 
-                <div class="card-footer d-flex justify-content-around">
-                    <button type="button" class="btn btn-outline-secondary border-0"><i class="bi bi-house d-block"
-                            style="font-size: 1rem;"></i>
-                        mi piace</button>
+                <div class="card-footer container-fluid d-flex flex-wrap justify-content-evenly" ">
+                    <button type=" button" class="btn btn-outline-secondary border-0"><i class="bi bi-house d-block"
+                        style="font-size: 1rem;"></i>like</button>
                     <button type="button" class="btn btn-outline-secondary border-0"><i
-                            class="bi bi-pencil-square d-block" style="font-size: 1rem;"></i>
-                        commento</button>
-                    <button type="button" class="btn btn-outline-secondary border-0 d-block" style="font-size: 1rem;"><i
-                            class="bi bi-shared-block" style="font-size: 1rem;"></i>
-                        condividi</button>
+                            class="bi bi-pencil-square d-block" style="font-size: 1rem;"></i>comment</button>
+                    <button type="button" class="btn btn-outline-secondary border-0" style="font-size: 1rem;"><i
+                            class="bi bi-share d-block" style="font-size: 1rem;"></i>share</button>
                     <button class="btn btn-outline-secondary border-0" type="button" data-bs-toggle="collapse"
                         data-bs-target=".multi-collapse" aria-expanded="false"
-                        aria-controls="multiCollapseExample1">mostra
-                        commenti</button>
+                        aria-controls="multiCollapseExample1">comment</button>
+                </div>
+                <div class="collapse multi-collapse" id="multiCollapseExample1">
+                    <div class="card m-2">
+                        <div class="card-header">
+                            <p class="card-text">Utente</p>
+                        </div>
+                        <div class="card card-body">
+                            <p class="card-text">commento</p>
+                        </div>
+                    </div>
+                    <div class="card m-2">
+                        <div class="card-header">
+                            <p class="card-text">Utente</p>
+                        </div>
+                        <div class="card card-body">
+                            <p class="card-text">commento</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
@@ -148,7 +159,9 @@ require_once 'checkSession.php';
             </ul>
         </aside>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
 </body>
 
 
