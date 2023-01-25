0c1742dbe65c735b5c40f265f1161b992f95ad3d
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-01-25 15:20:29 +0100

    fix post buttons

--- src/index.php
+++ src/index.php
@@ -99,11 +99,14 @@
                     class="object-fit-contain" alt="..." height="455" />
 
                 <div class="card-footer d-flex justify-content-around">
-                    <button type="button" class="btn btn-outline-secondary border-0"><i class="bi bi-house"></i>
+                    <button type="button" class="btn btn-outline-secondary border-0"><i class="bi bi-house d-block"
+                            style="font-size: 1rem;"></i>
                         mi piace</button>
-                    <button type="button" class="btn btn-outline-secondary border-0"><i class="bi bi-pencil-square"></i>
+                    <button type="button" class="btn btn-outline-secondary border-0"><i
+                            class="bi bi-pencil-square d-block" style="font-size: 1rem;"></i>
                         commento</button>
-                    <button type="button" class="btn btn-outline-secondary border-0"><i class="bi bi-share"></i>
+                    <button type="button" class="btn btn-outline-secondary border-0 d-block" style="font-size: 1rem;"><i
+                            class="bi bi-shared-block" style="font-size: 1rem;"></i>
                         condividi</button>
                     <button class="btn btn-outline-secondary border-0" type="button" data-bs-toggle="collapse"
                         data-bs-target=".multi-collapse" aria-expanded="false"
