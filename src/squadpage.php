8182fd4eab9ab1f4e91c8a972730a77d70ae75ac
Mattia Panni <pnmattia@gmail.com>
2023-01-26 18:02:11 +0100

    Add ui to squadpage, missing edit button, members and creator

--- src/squadpage.php
+++ src/squadpage.php
@@ -21,16 +21,25 @@ if (isset($_GET['name'])) {
 <body class="d-flex flex-column vh-100 " data-bs-theme="dark">
     <?php require_once 'templates/navbar.php'; ?>
 
-
     <main class="d-lg-flex flex-wrap justify-content-center overflow-auto vh-100">
         <aside
-            class="col-12 col-lg-3 p-3 shadow sticky-lg-top mh-100 overflow-auto d-flex flex-lg-column text-nowrap z-1">
+            class="col-12 col-lg-3 p-3 shadow sticky-lg-top mh-100 overflow-auto text-nowrap z-1">
+            <div class="d-flex">
+                <img src=<?php echo $dbh->getMediaUrl($squadProfile->getPicture()); ?> class="object-fit-contain rounded-circle p-2" alt="..." width="20%"/>
+                <div class="d-flex flex-column justify-content-evenly">
+                    <h5 class="mx-4"><?php echo $squadProfile->getName()?></h5>
+                    <p class="mx-4"><?php echo $squadProfile->getDescription()?></p>
+                </div>
+            </div>
+            <div class="d-flex flex-lg-column">
+                <a class="btn btn-secondary m-2" href="" type="button">Amici</a>
+                <a class="btn btn-secondary m-2" href="" type="button">Foto</a>                
+            </div>
             
         </aside>
-        <div class="col-12 col-lg-5 p-3 shadow">
-
+        <div class="col-12 col-lg-5 p-3 shadow mh-100">
+            <?php require 'templates/post.php'; ?>                            
         </div>
-        
     </main>
 
 </body>
