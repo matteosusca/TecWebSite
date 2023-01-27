8fda5ef40282138302deb297e277a0e67cbc323f
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-01-27 14:47:28 +0100

    refactor squadpage

--- src/squadpage.php
+++ src/squadpage.php
@@ -22,50 +22,30 @@ if (isset($_GET['name'])) {
 <body class="d-flex flex-column vh-100 " data-bs-theme="dark">
     <?php require_once 'templates/navbar.php'; ?>
     <main class="d-lg-flex flex-wrap justify-content-center overflow-auto vh-100">
-        <aside
-            class="col-12 col-lg-2 p-3 shadow sticky-lg-top mh-100 overflow-auto text-nowrap z-1">
+        <aside class="col-12 col-lg-2 p-3 shadow sticky-lg-top mh-100 overflow-auto text-nowrap z-1">
             <div class="d-flex">
-                <img src=<?php echo $dbh->getMediaUrl($squadProfile->getPicture()); ?> class="object-fit-contain rounded-circle p-2" alt="..." width="20%"/>
+                <img src=<?php echo $dbh->getMediaUrl($squadProfile->getPicture()); ?> class="object-fit-contain rounded-circle p-2" alt="..." width="20%" />
                 <div class="d-flex flex-column justify-content-evenly">
-                    <h5 class="mx-4"><?php echo $squadProfile->getName()?></h5>
-                    <p class="mx-4"><?php echo $squadProfile->getDescription()?></p>
+                    <h5 class="mx-4"><?php echo $squadProfile->getName() ?></h5>
+                    <p class="mx-4"><?php echo $squadProfile->getDescription() ?></p>
                 </div>
             </div>
-            <div class="d-flex flex-lg-column">            
+            <div class="d-flex flex-lg-column">
                 <form action="editsquad.php" method="post">
                     <input type="hidden" name="id" value=<?php echo $squadProfile->getId(); ?>>
                     <input class="btn btn-outline-secondary border-0" type="submit" name="edit_squad" value="Edit Squad">
                 </form>
             </div>
         </aside>
-        
+
         <div class="col-12 col-lg-4 p-3 shadow mh-100">
-            <?php require 'templates/post.php'; ?>                            
+            <?php require 'templates/post.php'; ?>
         </div>
-            <aside
-                class="col-12 col-lg-2 p-3 shadow sticky-lg-top mh-100 overflow-auto text-nowrap z-1">
-                <ul class="list-group list-group-flush offcanvas-body">
-                    <?php
-                        //get members of the squad and echo them
-                        foreach ($squadProfile->getMembers() as $member) {
-                            echo '<li class="list-group-item list-group-item-action">' . $member . '</li>';
-                        }
-                    ?>
-                </ul>
-            </aside>
+        <aside class="col-12 col-lg-2 p-3 shadow sticky-lg-top mh-100 overflow-auto text-nowrap z-1">
+            <?php require 'templates/members.php'; ?>
+        </aside>
     </main>
 </body>
 
 
 </html>
-
-
-
-
-
-
-
-
-
-
-
