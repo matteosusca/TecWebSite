eee59b6739d1451c0c53ce5382721dd41932a0ec
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-01-27 13:07:27 +0100

    add image in index for friends and squadds

--- src/templates/friends.php
+++ src/templates/friends.php
@@ -5,7 +5,11 @@
     //get friends of user from database and echo them
     $friends = $dbh->getFriends($_SESSION['username']);
     foreach ($friends as $friend) {
-        echo '<a class="list-group-item list-group-item-action" href="profile.php?user=' . $dbh->getUser($friend)->getUsername() . '">' . $dbh->getUser($friend)->getUsername() . '</a>';
+        echo '<a class="list-group-item list-group-item-action" href="profile.php?user=' . $dbh->getUser($friend)->getUsername() . '"> 
+        
+        <img src="' . $dbh->getMediaUrl($dbh->getUser($friend)->getProfilePicture()) . '" alt="" width="32" height="32" class="rounded-circle">'
+
+            . $dbh->getUser($friend)->getUsername() . '</a>';
     }
 
     ?>
