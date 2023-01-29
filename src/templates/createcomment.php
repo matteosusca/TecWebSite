df855cadf50467c078e991068c49f1a8ee16c1c3
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-01-29 15:27:42 +0100

    new interface post comment

--- src/templates/createcomment.php
+++ src/templates/createcomment.php
@@ -1,19 +1,22 @@
 <?php
 require_once 'bootstrap.php';
-if(isset($dbh) && isset($user) && isset($post)){
+if (isset($dbh) && isset($user) && isset($post)) {
     if (isset($_POST['submitComment'])) {
         $dbh->createComment($user->getUsername(), $post->getId(), $_POST['body']);
     }
-    ?>
-    
+?>
+
     <div class="card m-2">
         <div class="card-header ">
             <h5 class="card-title"> Crea commento</h5>
         </div>
         <div class="card-body">
             <form action="" method="post" enctype="multipart/form-data">
-                <input type=" text" class="form-control bg-body" id="body" placeholder="Scrivi un commento" name="body" required>
-                <button class="btn btn-outline-secondary w-100" type="submit" value="Pubblica" name="submitComment">Pubblica</button>
+                <div class="form-floating">
+                    <input type=" text" class="form-control bg-body" id="body" placeholder="Scrivi un commento" name="body" required>
+                    <label for="floatingInput">Scrivi un commento</label>
+                </div>
+                <button class="btn btn-outline-secondary mt-2 w-100" type="submit" value="Pubblica" name="submitComment">Pubblica</button>
             </form>
         </div>
     </div>
--- src/templates/createpost.php
+++ src/templates/createpost.php
@@ -3,17 +3,21 @@ if (!empty($_POST['submitPost'])) {
     $dbh->createPost($_SESSION['username'], $_POST['description'], $_FILES['postfile']);
 }
 ?>
-<div class="card m-2">
-    <div class="card-header ">
+<div class="card my-2">
+    <div class="card-header">
         <h5 class="card-title"> Crea post</h5>
     </div>
     <div class="card-body">
         <form action="index.php" method="post" enctype="multipart/form-data">
-            <input type=" text" class="form-control bg-body" id="description" placeholder="a cosa stai pensando?" name="description" required>
-            <button type="button" class="btn btn-outline-secondary border-0 disabled">aggiungi al tuo
-                post</button>
-            <input type="file" name="postfile" id="postfile" class="btn btn-outline-secondary border-0" required></input>
-            <button class="btn btn-outline-secondary w-100" type="submit" value="Pubblica" name="submitPost">Pubblica</button>
+            <div class="form-floating">
+                <input type=" text" class="form-control bg-body" id="description" placeholder="a cosa stai pensando?" name="description" required>
+                <label for="floatingInput">a cosa stai pensando?</label>
+            </div>
+            <div>
+                <label for="formFile" class="form-label mt-2">aggiungi al tuo post</label>
+                <input type="file" class="form-control bg-body" name="postfile" id="postfile" required>
+            </div>
+            <button class="btn btn-outline-secondary mt-3 w-100" type="submit" value="Pubblica" name="submitPost">Pubblica</button>
         </form>
     </div>
 </div>
