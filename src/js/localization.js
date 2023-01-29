c13edbca49c4babdce07c0a1c7221d8a2e59da3d
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-01-29 23:48:21 +0100

    fix friends position

--- src/js/localization.js
+++ src/js/localization.js
@@ -18,19 +18,19 @@ function initialize() {
                     console.log(response);
                 }
             });
+            $(document).ready(function () {
+                for (var i = 0; i < positions.length; i++) {
+                    var infowindow = new google.maps.InfoWindow({
+                        position: positions[i+1],
+                        map: map,
+                        content: positions[i]
+                    });
+                }
+            });
             var infowindow = new google.maps.InfoWindow({
                 map: map,
                 position: pos,
                 content: 'user location'
-            });
-            $(document).ready(function () {
-                for (var i = 0; i < positions.length; i++) {
-                    var infowindow = new google.maps.InfoWindow({
-                        position: new google.maps.LatLng(10.7143528, -74.0059731),
-                        map: map,
-                        content: positions[0]
-                    });
-                }
             });
             map.setCenter(pos);
         });
