8f832ba406e79c1ebe161902d7cb87af7b0a1ace
Mattia Panni <mattia.panni@studio.unibo.it>
2023-01-31 00:01:42 +0100

    Add UI to createsquad page

--- src/createsquad.php
+++ src/createsquad.php
@@ -1,25 +1,40 @@
 <?php
 require_once 'templates/head.php';
-if (!empty($_POST['submit'])) {
-    if ($dbh->createSquad($_POST['name'], $_POST['description'], $_SESSION['username'])) {
-        header("Location: squad.php?name=" . $_POST['name'] . "");
-    } else {
-        header("Location: createsquad.php?error=1");
-    }
+checkSession();
+
+if (!empty($_POST['save'])) {
+    $squad_id = $dbh->createSquad($_POST['name'], $_POST['description'], $_FILES['squadPicture'], $_SESSION['username']);
+    header("Location: squad.php?squad_id=" . $squad_id);
 }
 
 ?>
 
-<body>
-    <form action="createsquad.php" method="post">
-        <label for="name">Nome squad:</label>
-        <input type="text" id="name" name="name">
-        <br>
-        <label for="description">Descrizione:</label>
-        <textarea id="description" name="description" rows="4" cols="50"></textarea>
-        <br>
-        <input type="submit" value="Crea squadra">
-    </form>
+<body class="d-flex flex-column vh-100 " data-bs-theme="dark">
+    <?php require_once 'templates/navbar.php'; ?>
+    <main class="m-auto">
+        <div class="card">
+            <div class="card-header">
+                <h5>Create new squad</h5>
+            </div>
+            <div class="card-body">
+                <form action="createsquad.php" method="post">
+                    <div class="form-floating">
+                        <input type="text" class="form-control bg-body mt-2" id="name" placeholder="Nome squad" name="name">
+                        <label for="name">Nome squad</label>
+                    </div>
+                    <div class="form-floating">
+                        <textarea class="form-control bg-body mt-2" id="description" name="description" placeholder="Descrizione"></textarea>    
+                        <label for="description">Descrizione</label>
+                    </div>
+                    <div>
+                        <label for="formFile" class="form-label">Squad Picture</label>
+                        <input type="file" class="form-control bg-body" name="squadPicture" id="formFile">
+                    </div>
+                    <input class="btn btn-outline-secondary text-bg-dark mt-3 w-100" href="editsquad.php" type="submit" name="save" value="Save"></input>
+                </form>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
