6a0d46922aecd3dfc362e412f80dc0ffd49cfba8
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-01-31 15:21:20 +0100

    update js

--- src/templates/head.php
+++ src/templates/head.php
@@ -8,6 +8,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/style.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <title>SquadUp</title>
 </head>
