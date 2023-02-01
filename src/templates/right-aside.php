1c42bec38848c5baa0833027523d0fce53a62c7c
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-02-01 22:16:11 +0100

    refactor right-aside

--- src/templates/right-aside.php
+++ src/templates/right-aside.php
@@ -1,12 +1,13 @@
-<aside class="col-2 p-3 mh-100 shadow overflow-auto sticky-lg-top">
+<aside class="col-2 p-3 h-100 shadow overflow-auto sticky-lg-top">
     <div class="h-100 offcanvas-lg offcanvas-end" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
         <div class="offcanvas-header">
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
         </div>
-        <?php
-        if (basename($_SERVER['PHP_SELF']) == "index.php" || basename($_SERVER['PHP_SELF']) == "profile.php") { ?>
-            <div class="h-100">
-                <div class=" h-50 overflow-auto">
+
+        <div class="h-100 d-flex flex-column">
+            <?php
+            if (isset($templateParams['friends'])) { ?>
+                <div class="flex-fill overflow-auto">
                     <h5>Friends</h5>
                     <ul class="list-group list-group-flush offcanvas-body">
                         <?php foreach ($templateParams["friends"] as $friend) { ?>
@@ -17,7 +18,8 @@
                         ?>
                     </ul>
                 </div>
-                <div class=" h-50 overflow-auto">
+            <?php } if (isset($templateParams['squads'])) { ?>
+                <div class="flex-fill overflow-auto">
                     <h5>Squads</h5>
                     <ul class="list-group list-group-flush offcanvas-body">
                         <?php foreach ($templateParams["squads"] as $squad) { ?>
@@ -27,21 +29,20 @@
                         <?php } ?>
                     </ul>
                 </div>
-            </div>
-        <?php
-        } else if (basename($_SERVER['PHP_SELF']) == "squad.php") { ?>
-            <div class=" h-100 overflow-auto">
-                <h5>Members</h5>
-                <ul class="list-group list-group-flush offcanvas-body">
-                    <?php foreach ($templateParams["members"] as $member) { ?>
-                        <a class="list-group-item list-group-item-action" href="profile.php?user=<?php echo $member->getUsername() ?>">
-                            <img src=<?php echo $member->getprofilePicture() ?> alt="" width=" 32" height="32" class="rounded-circle">
-                            <?php echo $member->getUsername()  ?></a>
-                    <?php }
-                    ?>
-                </ul>
-            </div>
-        <?php
-        } ?>
+            <?php } if (isset($templateParams['members'])) { ?>
+                <div class="flex-fill overflow-auto">
+                    <h5>Members</h5>
+                    <ul class="list-group list-group-flush offcanvas-body">
+                        <?php foreach ($templateParams["members"] as $member) { ?>
+                            <a class="list-group-item list-group-item-action" href="profile.php?user=<?php echo $member->getUsername() ?>">
+                                <img src=<?php echo $member->getprofilePicture() ?> alt="" width=" 32" height="32" class="rounded-circle">
+                                <?php echo $member->getUsername()  ?></a>
+                        <?php }
+                        ?>
+                    </ul>
+                </div>
+            <?php
+            } ?>
+        </div>
     </div>
 </aside>
