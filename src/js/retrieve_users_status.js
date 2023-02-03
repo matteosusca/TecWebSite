9aedbab1ffeb4f77dc7f3d7048dd13b44bfc7db2
Mattia Panni <mattia.panni@studio.unibo.it>
2023-02-03 19:56:35 +0100

    Name refactor

--- src/js/retrieve_users_status.js
+++ src/js/retrieve_users_status.js
@@ -1,11 +1,11 @@
 function retrive_user_activity() {
     let usernames = Array.from(document.getElementsByName("user-username"));
     axios.get("api-get-active-users.php").then(response => {
-        usernames.forEach(username => { document.getElementById(username.value).innerHTML = getDate(response.data[username.value]) });
+        usernames.forEach(username => { document.getElementById(username.value).innerHTML = getStatus(response.data[username.value]) });
     });    
 }
 
-function getDate(last_activity) {
+function getStatus(last_activity) {
     return (Date.now() - new Date(last_activity)) <= 10_000 ? "Online" : "Last active: " + last_activity;
 }
 
