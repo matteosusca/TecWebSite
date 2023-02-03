eaa4f15df599376837c7b588084a94c88ac2e510
Mattia Panni <mattia.panni@studio.unibo.it>
2023-02-03 19:54:13 +0100

    Update method to get a string containing user status based on his last action

--- src/js/retrieve_users_status.js
+++ src/js/retrieve_users_status.js
@@ -1,8 +1,13 @@
 function retrive_user_activity() {
     let usernames = Array.from(document.getElementsByName("user-username"));
     axios.get("api-get-active-users.php").then(response => {
-        usernames.forEach(username => { document.getElementById(username.value).innerHTML = response.data[username.value] });
+        usernames.forEach(username => { document.getElementById(username.value).innerHTML = getDate(response.data[username.value]) });
     });    
 }
+
+function getDate(last_activity) {
+    return (Date.now() - new Date(last_activity)) <= 10_000 ? "Online" : "Last active: " + last_activity;
+}
+
 
 setInterval(() => { retrive_user_activity() }, 500); // get user status once every 5 seconds
