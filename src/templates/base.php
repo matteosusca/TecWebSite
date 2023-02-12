5340568815b5478b65f9f2c3468ed554ca53202f
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-02-12 10:06:42 +0100

    add libraries(js and css)

--- src/templates/base.php
+++ src/templates/base.php
@@ -11,8 +11,8 @@ if (checkSession()) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
     <link rel="stylesheet" href="css/style.css">
     <title><?php echo $templateParams["title"] ?></title>
 </head>
@@ -101,10 +101,11 @@ if (checkSession()) {
             <script src="<?php echo $script; ?>"></script>
     <?php endforeach;
     endif; ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <script src="js/set_active_user.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script src="js/set_active_user.js"></script>
     <script src="js/get_notification.js"></script>
+    
 </body>
 
 
