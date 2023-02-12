dfec1397aa2cf08b81e792a9637369c837db679a
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-02-12 12:01:24 +0100

    fix modal interface

--- src/templates/modal.php
+++ src/templates/modal.php
@@ -1,17 +1,16 @@
 <div class="modal fade" id="modalNotification" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5>Friend Request</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <!-- Two buttons to accept or decline the friend request -->
-                    <div class="d-flex justify-content-between">
-                        
-                        <button type="button" id="accept" class="btn btn-outline-success" data-bs-dismiss="modal">Accept</button>
-                        <button type="button" id="decline" class="btn btn-outline-danger" data-bs-dismiss="modal">Decline</button>
-                        <input type="hidden" id="sender">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>Friend Request</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- Two buttons to accept or decline the friend request -->
+                <div class="d-flex justify-content-between">
+                    <button type="button" id="accept" class="btn btn-outline-success" data-bs-dismiss="modal">Accept</button>
+                    <button type="button" id="decline" class="btn btn-outline-danger" data-bs-dismiss="modal">Decline</button>
+                    <input type="hidden" id="sender">
                 </div>
             </div>
         </div>
@@ -27,23 +26,21 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="card">
-                        <form action="" method="post" enctype="multipart/form-data">
-                    <div class="form-floating">
-                        <input type="text" class="form-control bg-body mt-2" id="name" placeholder="Nome squad" name="name" required>
-                        <label for="name">Nome squad</label>
-                    </div>
-                    <div class="form-floating">
-                        <textarea class="form-control bg-body mt-2" id="description" name="description" placeholder="Descrizione" required></textarea>
-                        <label for="description">Descrizione</label>
-                    </div>
-                    <div>
-                        <label for="squadPicture" class="form-label">Squad Picture</label>
-                        <input type="file" class="form-control bg-body" name="squadPicture" id="squadPicture" required>
-                    </div>
-                    <input class="btn btn-outline-secondary text-bg-dark mt-3 w-100" type="submit" name="save" value="Save">
-                </form>
-                    </div>
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div class="form-floating">
+                            <input type="text" class="form-control bg-body mt-2" id="name" placeholder="Nome squad" name="name" required>
+                            <label for="name">Nome squad</label>
+                        </div>
+                        <div class="form-floating">
+                            <textarea class="form-control bg-body mt-2" id="description" name="description" placeholder="Descrizione" required></textarea>
+                            <label for="description">Descrizione</label>
+                        </div>
+                        <div>
+                            <label for="squadPicture" class="form-label">Squad Picture</label>
+                            <input type="file" class="form-control bg-body" name="squadPicture" id="squadPicture" required>
+                        </div>
+                        <input class="btn btn-outline-secondary text-bg-dark mt-3 w-100" type="submit" name="save" value="Save">
+                    </form>
                 </div>
             </div>
         </div>
@@ -57,10 +54,10 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="card">
-                        <?php if (isset($_GET['error']) && $_GET['error'] == 1) { ?>
-                            <p>Mail already in use</p>
-                        <?php } ?><div class="modal-body">
+                    <?php if (isset($_GET['error']) && $_GET['error'] == 1) { ?>
+                        <p>Mail already in use</p>
+                    <?php } ?>
+                    <div class="modal-body">
                         <form action="" method="post" enctype="multipart/form-data">
                             <div>
                                 <label for="profilePicture" class="form-label">Profile Picture</label>
@@ -84,17 +81,15 @@
                 </div>
             </div>
         </div>
-    </div>
-<?php } else if (isset($templateParams['squad'])) {  ?>
-    <div class="modal fade" id="modalEditSquad" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5>Edit Squad</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="card">
+    <?php } else if (isset($templateParams['squad'])) {  ?>
+        <div class="modal fade" id="modalEditSquad" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5>Edit Squad</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
                         <form action="" method="post" enctype="multipart/form-data">
                             <input type="hidden" class="form-control" name="id" value=<?php echo $templateParams["squad"]->getID(); ?>>
                             <div>
@@ -113,11 +108,9 @@
                                 <label for="user" class="visually-hidden">User</label>
                                 <select name="user" class="col form-select bg-body mt-2  me-2" id="user">
                                     <option value="" disabled selected>Seleziona un utente</option>
-                                    <?php
-                                    foreach ($templateParams["squad"]->getMembers() as $user) {
+                                    <?php foreach ($templateParams["squad"]->getMembers() as $user) {
                                         echo "<option value='" . $user . "' >" . $user . "</option>";
-                                    }
-                                    ?>
+                                    } ?>
                                 </select>
                                 <label for="action" class="visually-hidden">Role for the user</label>
                                 <select name="action" class="col form-select bg-body mt-2" id="action">
@@ -132,34 +125,29 @@
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="modal fade" id="modalAddUserToSquad" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5>Add user</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="card">
+        <div class="modal fade" id="modalAddUserToSquad" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5>Add user</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
                         <form action="" method="post">
                             <input type="hidden" class="form-control" name="id" value=<?php echo $templateParams["squad"]->getID(); ?>>
                             <div class="row mx-0">
                                 <label for="user_friend" class="visually-hidden">Select friend to add</label>
                                 <select name="user_friend" class="btn btn-outline-secondary col-12" id="user_friend">
                                     <option value="" disabled selected>Seleziona un amico</option>
-                                    <?php
-                                    foreach ($templateParams["friends"] as $friend) {
+                                    <?php foreach ($templateParams["friends"] as $friend) {
                                         if (!$dbh->isUserMember($friend->getUsername(), $templateParams["squad"]->getId())) {
                                             echo "<option value='" . $friend->getUsername() . "' >" . $friend->getUsername() . "</option>";
                                         }
-                                    }
-                                    ?>
+                                    } ?>
                                 </select>
                             </div>
                             <button type="button" class="btn btn-outline-secondary border-0 disabled col-12">Or search for a user</button>
-
                             <div class="form-floating">
                                 <input type="text" class="form-control bg-body" name="searched_user" id="searched_user" placeholder="User">
                                 <label for="searched_user">User</label>
@@ -177,33 +165,28 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="modal fade" id="modalInviteUserToEvent" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5>Add user</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="card">
+        <div class="modal fade" id="modalInviteUserToEvent" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5>Add user</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
                         <form action="" method="post">
                             <input type="hidden" class="form-control" name="id" value=<?php echo $templateParams["squad"]->getID(); ?>>
                             <div class="row mx-0">
                                 <label for="user_friend" class="visually-hidden">Select friend to add</label>
                                 <select name="user_friend" class="btn btn-outline-secondary col-12" id="user_friend">
                                     <option value="" disabled selected>Seleziona un amico</option>
-                                    <?php
-                                    foreach ($templateParams["friends"] as $friend) {
+                                    <?php foreach ($templateParams["friends"] as $friend) {
                                         if (!$dbh->isUserMember($friend->getUsername(), $templateParams["squad"]->getId())) {
                                             echo "<option value='" . $friend->getUsername() . "' >" . $friend->getUsername() . "</option>";
                                         }
-                                    }
-                                    ?>
+                                    } ?>
                                 </select>
                             </div>
                             <button type="button" class="btn btn-outline-secondary border-0 disabled col-12">Or search for a user</button>
-
                             <div class="form-floating">
                                 <input type="text" class="form-control bg-body" name="searched_user" id="searched_user" placeholder="User">
                                 <label for="searched_user">User</label>
@@ -221,5 +204,4 @@
                 </div>
             </div>
         </div>
-    </div>
-<?php }  ?>
+    <?php }  ?>
