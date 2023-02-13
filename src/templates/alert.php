9ec5c5acf808077861a529590ad7937f9751a4bf
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-02-13 18:39:30 +0100

    fix alert

--- src/templates/alert.php
+++ src/templates/alert.php
@@ -1,1 +1,4 @@
-<div class='alert alert-danger position-fixed bottom-0 end-0 m-5' role='alert'><?php echo $templateParams["alert"] ?></div>
+<div class='alert alert-danger alert-dismissible fade show position-fixed bottom-0 end-0 m-5' role='alert'>
+    <?php echo $templateParams["alert"] ?>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
