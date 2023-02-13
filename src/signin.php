9dacf3b44927e01eb6418969373373ca9cbaaae6
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2023-02-13 09:58:32 +0100

    refactore last activity

--- src/signin.php
+++ src/signin.php
@@ -5,7 +5,6 @@ require_once 'bootstrap.php';
 if (!empty($_POST['submit'])) {
     if ($dbh->checkLogin($_POST['user'], $_POST['password'])) {
         $_SESSION['username'] = $_POST['user'];
-        $dbh->setLastActivity($_POST['user'], date("Y-m-d H:i:s", time()));
         header("Location: index.php");
     } else {
         header("Location: login.php?error=1");
